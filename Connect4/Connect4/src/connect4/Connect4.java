bb331a3099ffee7d2d7599e6167cf5a3a87ce446
wiesenthal <2006184@student.acalanes.k12.ca.us>
2017-03-16 11:40:06 -0700

    miles patch 0.1
    added getBoard, stringBoard, getSymbol

--- Connect4/Connect4/src/connect4/Connect4.java
+++ Connect4/Connect4/src/connect4/Connect4.java
@@ -12,7 +12,20 @@ public class Connect4 {
         // TODO code application logic here
     }
     
-    //getBoard
+    public int[][] getBoard(){
+        return board;
+    }
+    
+    public String stringBoard() {
+        String myBoard = "";
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j <board[0].length; j++){
+                myBoard += getSymbol(i,j) + "\t";
+            }
+            myBoard += "\n";
+        }
+        return myBoard;
+    }
     
     //askMove1
     
@@ -22,4 +35,14 @@ public class Connect4 {
     
     //checkWin
     
+    public String getSymbol(int row, int col) {
+        switch (board[row][col]) {
+            case 0:
+                return "-";
+            case -1:
+                return "O";
+            case 1:
+                return "●";
+        }
+    }
 }
