3d3056d122900263f1116dfdeec66d1e9c4c8519
wiesenthal <2006184@student.acalanes.k12.ca.us>
2017-03-16 11:24:58 -0700

    new

--- Connect4/Connect4/src/connect4/Connect4.java
+++ Connect4/Connect4/src/connect4/Connect4.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package connect4;
 
 /**
@@ -11,11 +6,20 @@ package connect4;
  */
 public class Connect4 {
 
-    /**
-     * @param args the command line arguments
-     */
+    private int[][] board = new int[6][7];
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
+    
+    //getBoard
+    
+    //askMove1
+    
+    //askMove2
+    
+    //checkValid
+    
+    //checkWin
     
 }
