16bf305622cf26f589425e69d1bcbc063b1fef17
wiesenthal <2006184@student.acalanes.k12.ca.us>
2017-03-17 11:18:42 -0700

    separate classes

--- Connect4/Connect4/src/connect4/Connect4.java
+++ Connect4/Connect4/src/connect4/Connect4.java
@@ -6,52 +6,11 @@ package connect4;
  */
 public class Connect4 {
 
-    private int[][] board = new int[6][7];
+    private Board board;
     
     public static void main(String[] args) {
         // TODO code application logic here
     }
+
     
-    public int[][] getBoard(){
-        return board;
-    }
-    
-    public String stringBoard() {
-        String myBoard = "";
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j <board[0].length; j++){
-                myBoard += getSymbol(i,j) + "\t";
-            }
-            myBoard += "\n";
-        }
-        return myBoard;
-    }
-    
-    //askMove1
-    
-    //askMove2
-    
-    public boolean checkValid(int col) {
-        for (int[] row : board) {
-            if (row[col] == 0) {
-                return true;
-            }
-        }
-        return false;
-    }
-    
-    //dropDown
-    
-    //checkWin
-    
-    public String getSymbol(int row, int col) {
-        switch (board[row][col]) {
-            case -1:
-                return "O";
-            case 1:
-                return "●";
-            default:
-                return "-";
-        }
-    }
 }
