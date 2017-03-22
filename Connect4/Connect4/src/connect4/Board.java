324388b0e14f75e1ce8dcc3f4a965c8fd840cf5c
2006663 <2006663@AL-105-35.student.acalanes.k12.ca.us>
2017-03-22 11:27:09 -0700

    Merge origin/Solution into Solution
    Conflicts:
    	src/Activity01/Card.java

---
+++ Connect4/Connect4/src/connect4/Board.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connect4;
+
+/**
+ *
+ * @author 2006184
+ */
+
+public class Board {
+    private int[][] board = new int[6][7];
+    public String getSymbol(int row, int col) {
+        switch (board[row][col]) {
+            case -1:
+                return "O";
+            case 1:
+                return "●";
+            default:
+                return "-";
+        }
+    }
+    public String toString() {
+        String myBoard = "";
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j <board[0].length; j++){
+                myBoard += getSymbol(i,j) + "\t";
+            }
+            myBoard += "\n";
+        }
+        return myBoard;
+    }
+    public boolean checkValid(int col) {
+        for (int[] row : board) {
+            if (row[col] == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    //checkWin
+    
+    public void makeMove(int col,int playerType) {
+        board[dropDown(int row)][col] = playerType;
+    }
+    
+    //dropDown
+    
+}
