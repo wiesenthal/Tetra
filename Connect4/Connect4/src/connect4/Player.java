e5225cbd579d33cfa8c34959aced9d35c0a42e27
2006663 <2006663@AL-105-35.student.acalanes.k12.ca.us>
2017-03-22 11:53:57 -0700

    Merge origin/Solution into Solution
    Conflicts:
    	src/Activity01/Card.java

--- Connect4/Connect4/src/connect4/Player.java
+++ Connect4/Connect4/src/connect4/Player.java
@@ -10,17 +10,27 @@ import java.util.Scanner;
  * @author 2006663
  */
 public class Player {
+    
     int playerNum; //-1 or 1
     private Board theBoard;
+    
+    public Player(int pType, Board b) {
+        playerNum = pType;
+        theBoard = b;
+    }
     public void askMove() {
-        System.out.println("Which column would you like to move in?");
-        Scanner scan = new Scanner(System.in);
-        int col = scan.nextInt();
-        if (theBoard.checkValid(col)) {
+        if (playerNum==2) {
+            
+        } else {
+            System.out.println("Which column would you like to move in?");
+            Scanner scan = new Scanner(System.in);
+            int col = scan.nextInt();
+            if (theBoard.checkValid(col)) {
             theBoard.makeMove(col, playerNum);
-        }
-        else {
+            }
+            else {
             System.out.println("Invalid move, different spot?");
+            }
         }
     }
 }
