1a502e99a7f7848552a6aa91d1698dde40bf1f93
2006893 <2006893@AL-105-33.student.acalanes.k12.ca.us>
2017-03-22 11:58:11 -0700

    Activity 2 completed

--- Connect4/Connect4/src/connect4/Player.java
+++ Connect4/Connect4/src/connect4/Player.java
@@ -1,27 +1,36 @@
-package connect4;
-import java.util.Scanner;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+package connect4;
+import java.util.Scanner;
 /**
  *
- * @author 2006893
+ * @author 2006663
  */
 public class Player {
+    
+    int playerNum; //-1 or 1
     private Board theBoard;
+    
+    public Player(int pType, Board b) {
+        playerNum = pType;
+        theBoard = b;
+    }
     public void askMove() {
-        System.out.println("What row would you like to place a tile?");
-        Scanner scan = new Scanner(System.in);
-        int move = scan.nextInt();
-        theBoard.checkValid(move);
-        if (theBoard.checkValid(move)) {
-            //theBoard.makeMove();
-        }
-        else {
-            System.out.println("Invalid Position. Choose again.");
+        if (playerNum==2) {
+            
+        } else {
+            System.out.println("Which column would you like to move in?");
+            Scanner scan = new Scanner(System.in);
+            int col = scan.nextInt();
+            if (theBoard.checkValid(col)) {
+            theBoard.makeMove(col, playerNum);
+            }
+            else {
+            System.out.println("Invalid move, different spot?");
+            }
         }
     }
 }
