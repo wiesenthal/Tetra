1a0517487fead883422fda86d56dda004966dd13
2006893 <2006893@AL-105-33.student.acalanes.k12.ca.us>
2017-03-27 11:04:49 -0700

    Activity 2 completed

--- Connect4/Connect4/src/connect4/Player.java
+++ Connect4/Connect4/src/connect4/Player.java
@@ -15,22 +15,54 @@ public class Player {
     private Board theBoard;
     
     public Player(int pType, Board b) {
-        playerNum = pType;
+        playerNum = pType; // 1 for player 1, -1 for player 2
         theBoard = b;
     }
     public void askMove() {
         if (playerNum==2) {
-            
+            theBoard.makeMove(aiMove(theBoard.getBoard()), -1);
         } else {
             System.out.println("Which column would you like to move in?");
             Scanner scan = new Scanner(System.in);
             int col = scan.nextInt();
             if (theBoard.checkValid(col)) {
-            theBoard.makeMove(col, playerNum);
+                theBoard.makeMove(col, playerNum);
             }
             else {
             System.out.println("Invalid move, different spot?");
             }
         }
+    }
+    private int aiMove(int[][] bort, int num) { //num is which player it is
+        Board board = new Board(bort);
+        if (board.checkWin(-1)) { //AI wins
+            return 1;
+        }
+        else if (board.checkWin(1)) { //Player wins
+            return -1;
+        }
+        else { //Not base case
+            int[] slots = new int[8];
+            for (int i = 0; i < slots.length; i++) {
+                
+                // if the column is full, make it unusable
+                if (!board.checkValid(i)) {
+                    slots[i] = -2;
+                }
+                //for ai best move
+                if (slots[i] != -2) { 
+                    slots[i] = aiMove(testBoard(i, num, bort), num*-1);     
+                }
+
+            }
+            
+        }
+    }
+    
+    //creates a theoretical boardstate
+    private int[][] testBoard(int col, int p, int[][] bort) {
+        Board board = new Board(bort);
+        board.makeMove(col, p);
+        return board.getBoard();
     }
 }
